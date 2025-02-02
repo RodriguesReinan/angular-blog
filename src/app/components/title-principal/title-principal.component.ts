65a67f8ef9dcb828d1a2e0923ae8c4431b8a1555
RodriguesReinan <reinanro.rp@gmail.com>
2025-02-02 15:26:49 -0300

    components styles and created

---
+++ src/app/components/title-principal/title-principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-principal',
+  templateUrl: './title-principal.component.html',
+  styleUrls: ['./title-principal.component.css']
+})
+export class TitlePrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
