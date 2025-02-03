57ca1429605616a1341a440f218a0ce77ed371ee
RodriguesReinan <reinanro.rp@gmail.com>
2025-02-02 23:06:16 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOTÍCIA 01",
+    "description":"Olá eu sou o id 1",
+    "photoCover": "https://renovainvest.com.br/wp-content/uploads/2022/06/5-Motivos-para-ter-Ethereum-ETH-na-carteira-v2.png.webp"
+},
+{
+  "id":"2",
+  "title":"NOTÍCIA 02",
+  "description":"Olá eu sou o id 2",
+  "photoCover": "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/12832/production/_99262857_gettyimages-881768862.jpg.webp"
+}
+]
